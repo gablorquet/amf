ec81fd071bf30a67deaa7017df383574a4dabdc8
TheBass <bass.lavoie@gmail.com>
2014-11-10 20:36:30 -0500

    created CiController.cs

---
+++ amf/Controllers/CiController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Core.Extensions;
+using Core.Models;
+
+namespace amf.Controllers
+{
+    public class CiController : Controller
+    {
+        public string Index()
+        {
+            var bass = new Animateur
+            {
+                Email = "bass.lavoie@gmail.com",
+                Name = "Bass Lavoie",
+                Password = "patate".ToSHA1(),
+                Username = "The_Bass"
+            };
+
+            return "Done";
+        }
+
+    }
+}
